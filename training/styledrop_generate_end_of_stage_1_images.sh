32aacd341490759cf21706bc61b83a997d3a00e7
Will Berman <WLBberman@gmail.com>
2023-12-01 07:43:10 +0000

    remove

--- training/styledrop_generate_end_of_stage_1_images.sh
+++
@@ -1,4 +0,0 @@
-python styledrop_generate_end_of_stage_1_images.py \
-    --pretrained_model_name_or_path openMUSE/diffusers-pipeline \
-    --write_images_to ../styledrop_end_of_stage_1_images/ \
-    --load_transformer_from /fsx/william/styledrop
